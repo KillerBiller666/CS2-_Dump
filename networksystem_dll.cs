a13497b472cd9892238e48346d392dffe982a4cf
KillerBiller666 <143535139+KillerBiller666@users.noreply.github.com>
2026-08-12 02:37:31 +0200

    Newest dump

--- networksystem_dll.cs
+++ networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-03 21:31:19.397370 UTC
+// 2026-08-12 00:35:48.146160100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
