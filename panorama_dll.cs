4a421e2df809f56a48303669dac780b35c56b79e
KillerBiller666 <143535139+KillerBiller666@users.noreply.github.com>
2026-08-20 02:34:40 +0200

    Dump update

--- panorama_dll.cs
+++ panorama_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-12 00:35:48.146160100 UTC
+// 2026-08-20 00:33:11.692378500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: panorama.dll
--- panorama_dll.zig
+++ panorama_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-12 00:35:48.146160100 UTC
+// 2026-08-20 00:33:11.692378500 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
